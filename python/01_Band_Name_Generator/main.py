1746b9aa2d454f8fbfaeba5d894f764304720938
oliverjhernandez <oliverjhernandez@gmail.com>
2024-01-19 08:14:55 -0500

    Python init

---
+++ python/01_Band_Name_Generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of your city?\n")
+pet = input("What's the name of your pet?\n")
+
+
+print(f"Your band could be named {city} {pet}")
