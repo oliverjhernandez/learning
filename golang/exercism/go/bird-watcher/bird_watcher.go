903d5a42c67c50f2434d4ca8e1a6dbab4a2e74c1
oliverjhernandez <oliverjhernandez@gmail.com>
2024-04-05 15:06:52 -0500

    go: add bird-watcher exercise

---
+++ golang/exercism/go/bird-watcher/bird_watcher.go
@@ -0,0 +1,34 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	var total int
+	for i := 0; i < len(birdsPerDay); i++ {
+		total += birdsPerDay[i]
+	}
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	var total int
+	inital := ((week - 1) * 7)
+	ending := (week * 7)
+	weekDays := birdsPerDay[inital:ending]
+
+	for i := 0; i < len(weekDays); i++ {
+		total += weekDays[i]
+	}
+	return total
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i] += 1
+	}
+	return birdsPerDay
+}
