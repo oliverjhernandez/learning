e9e128d62c704de5f702e03d4e67b80d61f30059
oliverjhernandez <oliverjhernandez@gmail.com>
2024-04-19 18:22:02 -0500

    chore: moved exercism to new directory

---
+++ exercism/go/strain/strain.go
@@ -0,0 +1,11 @@
+package strain
+
+type Filter func(int) bool
+
+// Implement the "Keep" and "Discard" function in this file.
+func Keep(cp []T, p Filter) {
+}
+
+// You will need typed parameters (aka "Generics") to solve this exercise.
+// They are not part of the Exercism syllabus yet but you can learn about
+// them here: https://go.dev/tour/generics/1
