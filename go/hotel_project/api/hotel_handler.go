e7253cd929daa1c327574ac9b9e133c3c900a3b7
oliverjhernandez <oliverjhernandez@gmail.com>
2024-05-27 17:38:18 -0500

    go: hotel-reservation | add hotel handler

---
+++ go/hotel_project/api/hotel_handler.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"fmt"
+
+	"hotel/db"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type HotelHandler struct {
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+}
+
+type HotelQueryParms struct {
+	Rooms  bool
+	Rating int
+}
+
+func (hs *HotelHandler) HandlerGetHotels(c *fiber.Ctx) error {
+	var qparams HotelQueryParms
+	if err := c.QueryParser(&qparams); err != nil {
+		return err
+	}
+
+	hotels, err := hs.hotelStore.GetHotels(c.Context(), nil)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(hotels)
+}
+
+func NewHotelHandler(hs db.HotelStore, rs db.RoomStore) *HotelHandler {
+	return &HotelHandler{
+		hotelStore: hs,
+		roomStore:  rs,
+	}
+}
--- go/hotel_project/db/hotel_store.go
+++ go/hotel_project/db/hotel_store.go
@@ -15,6 +15,7 @@ const (
 )
 
 type HotelStore interface {
+	GetHotels(ctx context.Context, filter bson.M) ([]*types.Hotel, error)
 	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
 	UpdateHotel(ctx context.Context, filter bson.M, values bson.M) error
 }
@@ -22,6 +23,20 @@ type HotelStore interface {
 type MongoHotelStore struct {
 	client     *mongo.Client
 	collection *mongo.Collection
+}
+
+func (hs *MongoHotelStore) GetHotels(ctx context.Context, filter bson.M) ([]*types.Hotel, error) {
+	resp, err := hs.collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	var hotels []*types.Hotel
+	if err := resp.All(ctx, &hotels); err != nil {
+		return nil, err
+	}
+
+	return hotels, nil
 }
 
 func (hs *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
