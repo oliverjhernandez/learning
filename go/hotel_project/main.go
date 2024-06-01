db7d03bd3b53ba3b64e13a30f7c886e5c4178a01
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-01 12:33:36 -0500

    go: hotel-reservation | implements all new stores and handlers

--- go/hotel_project/main.go
+++ go/hotel_project/main.go
@@ -29,20 +29,26 @@ func main() {
 	var (
 		listenAddr = flag.String("listenAddr", ":3000", "The listen address of the API server")
 		app        = fiber.New(config)
-		auth       = app.Group("/api")
-		appv1      = app.Group("/v1", middleware.JWTAuthentication)
 
-		userStore  = db.NewMongoUserStore(client, db.DBNAME)
-		hotelStore = db.NewMongoHotelStore(client, db.DBNAME)
-		roomStore  = db.NewMongoRoomStore(client, hotelStore, db.DBNAME)
-		store      = &db.Store{
-			User:  userStore,
-			Hotel: hotelStore,
-			Room:  roomStore,
+		userStore    = db.NewMongoUserStore(client, db.DBNAME)
+		hotelStore   = db.NewMongoHotelStore(client, db.DBNAME)
+		bookingStore = db.NewMongoBookingStore(client, db.DBNAME)
+		roomStore    = db.NewMongoRoomStore(client, hotelStore, db.DBNAME)
+		store        = &db.Store{
+			User:    userStore,
+			Hotel:   hotelStore,
+			Room:    roomStore,
+			Booking: bookingStore,
 		}
-		authHandler  = api.NewAuthHandler(userStore)
-		hotelHandler = api.NewHotelHandler(store)
-		userHandler  = api.NewUserHandler(store)
+		authHandler    = api.NewAuthHandler(userStore)
+		roomHandler    = api.NewBookRoomHandler(store)
+		hotelHandler   = api.NewHotelHandler(store)
+		bookingHandler = api.NewBookingHandler(store)
+		userHandler    = api.NewUserHandler(store)
+
+		appv1 = app.Group("/v1", middleware.JWTAuthentication(store.User))
+		auth  = app.Group("/api")
+		admin = appv1.Group("/admin", middleware.AdminAuth)
 	)
 
 	// Auth
@@ -60,6 +66,15 @@ func main() {
 	appv1.Get("/hotel", hotelHandler.HandlerGetHotels)
 	appv1.Get("/hotel/:id", hotelHandler.HandlerGetHotel)
 	appv1.Get("/hotels/:id/rooms", hotelHandler.HandlerGetRooms)
+
+	// Rooms
+	appv1.Get("/room", roomHandler.HandleGetRooms)
+	appv1.Post("/room/:id/book", roomHandler.HandleBookRoom)
+
+	// Bookings
+	// TODO: cancel booking
+	admin.Get("/booking", bookingHandler.HandlerGetBookings)
+	appv1.Get("/booking/:id", bookingHandler.HandlerGetBooking)
 
 	app.Listen(*listenAddr)
 }
