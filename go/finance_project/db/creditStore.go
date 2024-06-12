6b6859393c3fe8311467fcd8b0dd03031642557b
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-12 16:55:59 -0500

    go: finance | add creditStore

---
+++ go/finance_project/db/creditStore.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"context"
+
+	"finance/types"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	creditsCollection = "credits"
+)
+
+type MongoCreditStore struct {
+	client     *mongo.Client
+	collection *mongo.Collection
+	dbname     string
+}
+
+func (cs *MongoCreditStore) GetCredits(ctx context.Context) ([]*types.Credit, error) {
+	cur, err := cs.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	var credits []*types.Credit
+	if err := cur.All(ctx, &credits); err != nil {
+		return nil, err
+	}
+	return credits, nil
+}
+
+func (cs *MongoCreditStore) GetCredityID(ctx context.Context, id string) (*types.Credit, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.M{
+		"_id": oid,
+	}
+
+	var credit types.Credit
+	if err := cs.collection.FindOne(ctx, filter).Decode(&credit); err != nil {
+		return nil, err
+	}
+
+	return &credit, nil
+}
+
+func (cs *MongoCreditStore) InsertCredit(ctx context.Context, cred *types.Credit) (*types.Credit, error) {
+	res, err := cs.collection.InsertOne(ctx, cred)
+	if err != nil {
+		return nil, err
+	}
+	cred.ID = res.InsertedID.(primitive.ObjectID)
+
+	return cred, nil
+}
+
+func (cs *MongoCreditStore) UpdateCredit(ctx context.Context, filter Params, update *types.UpdateCreditParams) error {
+	updateQuery := bson.D{
+		primitive.E{
+			Key: "$set", Value: update,
+		},
+	}
+	_, err := cs.collection.UpdateOne(ctx, filter, updateQuery)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cs *MongoCreditStore) DeleteCreditByID(ctx context.Context, id string) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": oid}
+	_, err = cs.collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+	return nil
+}
