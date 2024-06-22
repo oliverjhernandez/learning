1d61e72a48d235d3e94aeb8828bf049637dc6f1e
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-22 12:27:51 -0500

    go: finance | updated seeds script

--- go/finance_project/scripts/seeds.go
+++ go/finance_project/scripts/seeds.go
@@ -3,17 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"finance/db"
-	"finance/types"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"finance/models"
 )
 
 var (
-	client      *mongo.Client
 	ctx         = context.Background()
 	txStore     db.TransactionStore
 	userStore   db.UserStore
@@ -21,117 +18,99 @@ var (
 )
 
 func seed() {
-	transactions := []*types.Transaction{
+	transactions := []*models.Transaction{
 		{
-			TransactionBase: types.TransactionBase{
-				Concept:     "Supermercado",
-				Description: "Desayuno de la semana",
-				Value:       320000,
-				Date:        1716481693,
-				Relevance:   types.Essential,
-				Currency:    types.USD,
-				Account:     types.SAVINGS,
-			},
+			Concept:     "Supermercado",
+			Description: "Desayuno de la semana",
+			Value:       320000,
+			Date:        1716481693,
+			Relevance:   models.Essential,
+			Currency:    models.USD,
+			Account:     models.SAVINGS,
 		},
 		{
-			TransactionBase: types.TransactionBase{
-				Concept:     "Actividades",
-				Description: "Natación",
-				Value:       20000,
-				Date:        1746381693,
-				Relevance:   types.Optional,
-				Currency:    types.USD,
-				Account:     types.SAVINGS,
-			},
+			Concept:     "Actividades",
+			Description: "Natación",
+			Value:       20000,
+			Date:        1746381693,
+			Relevance:   models.Optional,
+			Currency:    models.USD,
+			Account:     models.SAVINGS,
 		},
 		{
-			TransactionBase: types.TransactionBase{
-				Concept:     "Ahorro",
-				Description: "Mensual",
-				Value:       100000,
-				Date:        1746381693,
-				Relevance:   types.Important,
-				Currency:    types.USD,
-				Account:     types.SAVINGS,
-			},
+			Concept:     "Ahorro",
+			Description: "Mensual",
+			Value:       100000,
+			Date:        1746381693,
+			Relevance:   models.Important,
+			Currency:    models.USD,
+			Account:     models.SAVINGS,
 		},
 		{
-			TransactionBase: types.TransactionBase{
-				Concept:     "Ropa",
-				Description: "Playa",
-				Value:       5000,
-				Date:        1746381693,
-				Relevance:   types.Optional,
-				Currency:    types.USD,
-				Account:     types.SAVINGS,
-			},
+			Concept:     "Ropa",
+			Description: "Playa",
+			Value:       5000,
+			Date:        1746381693,
+			Relevance:   models.Optional,
+			Currency:    models.USD,
+			Account:     models.SAVINGS,
 		},
 	}
 
-	users := []*types.User{
+	users := []*models.User{
 		{
-			UserBase: types.UserBase{
-				FirstName: "Corina",
-				LastName:  "Pulido",
-				Email:     "corinapulido@gmail.com",
-			},
-			Passwd: "123567qwerty",
+			FirstName: "Corina",
+			LastName:  "Pulido",
+			Email:     "corinapulido@gmail.com",
+			Passwd:    "123567qwerty",
 		},
 		{
-			UserBase: types.UserBase{
-				FirstName: "Oliver",
-				LastName:  "Hernandez",
-				Email:     "oliverjhernandez@gmail.com",
-			},
-			Passwd: "123567qwerty",
+			FirstName: "Oliver",
+			LastName:  "Hernandez",
+			Email:     "oliverjhernandez@gmail.com",
+			Passwd:    "123567qwerty",
 		},
 		{
-			UserBase: types.UserBase{
-				FirstName: "Chuck",
-				LastName:  "Norris",
-				Email:     "notascratch@gmail.com",
-			},
-			Passwd: "123567qwerty",
+			FirstName: "Chuck",
+			LastName:  "Norris",
+			Email:     "notascratch@gmail.com",
+			Passwd:    "123567qwerty",
 		},
 	}
 
-	credits := []*types.Credit{
+	credits := []*models.Credit{
 		{
-			CreditBase: types.CreditBase{
-				ClosingDate:         time.Now(),
-				DueDate:             time.Now().Add(time.Hour * 24 * 365),
-				Entity:              types.BANCOLOMBIA,
-				Identifier:          "q3hf489657439-42f89h5",
-				Type:                types.HIPOTECARIO,
-				Rate:                2.14,
-				Total:               80000000,
-				Number_Installments: 12,
-			},
+			ClosingDate:  time.Now(),
+			DueDate:      time.Now().Add(time.Hour * 24 * 365),
+			Entity:       models.BANCOLOMBIA,
+			Identifier:   "q3hf489657439-42f89h5",
+			Type:         models.HIPOTECARIO,
+			Rate:         2.14,
+			Total:        80000000,
+			Installments: 12,
 		},
 		{
-			CreditBase: types.CreditBase{
-				ClosingDate:         time.Now(),
-				DueDate:             time.Now().Add(time.Hour * 24 * 365 * 4),
-				Entity:              types.AV_VILLAS,
-				Identifier:          "uofhehuiwrhurwfghvrw-34678",
-				Type:                types.LIBRE_INVERSION,
-				Rate:                1.8,
-				Total:               10000000,
-				Number_Installments: 24,
-			},
+			ClosingDate:  time.Now(),
+			DueDate:      time.Now().Add(time.Hour * 24 * 365 * 4),
+			Entity:       models.AV_VILLAS,
+			Identifier:   "uofhehuiwrhurwfghvrw-34678",
+			Type:         models.LIBRE_INVERSION,
+			Rate:         1.8,
+			Total:        10000000,
+			Installments: 24,
 		},
 	}
 
 	for _, v := range transactions {
-		txStore.InsertTransaction(ctx, v)
+		txStore.InsertTransaction(ctx, nil, v)
 	}
 
 	for _, v := range users {
-		userStore.InsertUser(ctx, v)
+		userStore.InsertUser(ctx, nil, v)
 	}
 
 	for _, v := range credits {
-		creditStore.InsertCredit(ctx, v)
+		creditStore.InsertCredit(ctx, nil, v)
 	}
 }
 
@@ -140,15 +119,14 @@ func main() {
 }
 
 func init() {
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(db.DBURI))
+	_, client, err := db.NewStore()
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("initialization failed: %v", err)
+		os.Exit(1)
 	}
+	defer client.Close()
 
-	if err := client.Database(db.TDBNAME).Drop(ctx); err != nil {
-		log.Fatal(err)
-	}
-	txStore = db.NewMongoTransactionStore(client, db.TDBNAME)
-	userStore = db.NewMongoUserStore(client, db.TDBNAME)
-	creditStore = db.NewMongoCreditStore(client, db.TDBNAME)
+	txStore = db.NewPGTransactionStore(client)
+	userStore = db.NewPGUserStore(client)
+	creditStore = db.NewPGCreditStore(client)
 }
