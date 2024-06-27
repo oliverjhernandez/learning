dbd0d24a7a1c9b7971d8f4cad6f8c39d24effdc4
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-27 18:08:33 -0500

    go: finance | reorder main.go file

--- go/finance_project/main.go
+++ go/finance_project/main.go
@@ -10,38 +10,34 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-const (
-	dburi = "mongodb://localhost:27017"
-	pg_db = ""
-)
-
-var config = fiber.Config{
-	ErrorHandler: api.ErrorHandler,
-}
-
 func main() {
-	store, client, err := db.NewStore()
+	stores, client, err := db.NewStore()
 	if err != nil {
 		fmt.Printf("error loading db store")
 	}
 	defer client.Close()
 
-	app := fiber.New(config)
-	appv1 := app.Group("/v1")
-
-	appv1.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
 	var (
+		config     = fiber.Config{ErrorHandler: api.ErrorHandler}
+		app        = fiber.New(config)
 		listenAddr = flag.String("listenAddr", ":3000", "The listen address of the API server")
-		// store, _   = db.NewStore(client)
 
 		// Handlers
-		txHandler     = api.NewTransactionHandler(store)
-		userHandler   = api.NewUserHandler(store)
-		creditHandler = api.NewCreditHandler(store)
+		// TODO: restrict each handler ot its specific store
+		txHandler      = api.NewTransactionHandler(stores)
+		userHandler    = api.NewUserHandler(stores)
+		creditHandler  = api.NewCreditHandler(stores)
+		accountHandler = api.NewAccountHandler(stores)
+		authHandler    = api.NewAuthHandler(stores)
+
+		// appv1 := app.Group("/v1")
+		appv1 = app.Group("/v1", api.JWTAuthentication(stores.UserStore))
+		auth  = app.Group("/api")
+		// admin = appv1.Group("/admin", api.AdminAuth)
 	)
+
+	// Auth
+	auth.Post("/auth", authHandler.HandleAuthenticate)
 
 	// Transaction CRUD Endpoints
 	appv1.Get("/transaction", txHandler.HandlerGetTransactions)
@@ -63,6 +59,13 @@ func main() {
 	appv1.Post("/user", userHandler.HandlerPostUser)
 	appv1.Delete("/user/:id", userHandler.HandlerDeleteUser)
 	appv1.Patch("/user/:id", userHandler.HandlerUpdateUser)
+
+	// Account CRUD Endpoints
+	appv1.Get("/account", accountHandler.HandlerGetAccounts)
+	appv1.Get("/account/:id", accountHandler.HandlerGetAccount)
+	appv1.Post("/account", accountHandler.HandlerPostAccount)
+	appv1.Delete("/account/:id", accountHandler.HandlerDeleteAccount)
+	appv1.Patch("/account/:id", accountHandler.HandlerUpdateAccount)
 
 	app.Listen(*listenAddr)
 }
