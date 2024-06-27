3e21c48a3a8b6e8de4e4ae77b1c1527583255df7
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-27 16:42:15 -0500

    go: finance | reorderd user handler

--- go/finance_project/api/user_handler.go
+++ go/finance_project/api/user_handler.go
@@ -19,6 +19,25 @@ func NewUserHandler(s *db.Store) *UserHandler {
 	}
 }
 
+func (uh *UserHandler) HandlerPostUser(c *fiber.Ctx) error {
+	var params models.CreateUser
+	if err := c.BodyParser(&params); err != nil {
+		return ErrInvalidReqBody()
+	}
+
+	user, err := models.NewUserFromParams(&params)
+	if err != nil {
+		return err
+	}
+
+	res, err := uh.Store.InsertUser(c.Context(), nil, user)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(res)
+}
+
 func (uh *UserHandler) HandlerGetUsers(c *fiber.Ctx) error {
 	users, err := uh.Store.GetAllUsers(c.Context(), nil)
 	if err != nil {
@@ -41,36 +60,6 @@ func (uh *UserHandler) HandlerGetUser(c *fiber.Ctx) error {
 	return c.JSON(user)
 }
 
-func (uh *UserHandler) HandlerPostUser(c *fiber.Ctx) error {
-	var params models.CreateUser
-	if err := c.BodyParser(&params); err != nil {
-		return ErrInvalidReqBody()
-	}
-
-	user := models.NewUserFromParams(params)
-
-	res, err := uh.Store.InsertUser(c.Context(), nil, user)
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(res)
-}
-
-func (uh *UserHandler) HandlerDeleteUser(c *fiber.Ctx) error {
-	strID := c.Params("id")
-	id, err := strconv.Atoi(strID)
-	if err != nil {
-		return err
-	}
-
-	if err := uh.Store.DeleteUserByID(c.Context(), nil, id); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func (uh *UserHandler) HandlerUpdateUser(c *fiber.Ctx) error {
 	strID := c.Params("id")
 	id, err := strconv.Atoi(strID)
@@ -86,5 +75,19 @@ func (uh *UserHandler) HandlerUpdateUser(c *fiber.Ctx) error {
 	if err := uh.Store.UpdateUser(c.Context(), nil, id, &params); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (uh *UserHandler) HandlerDeleteUser(c *fiber.Ctx) error {
+	strID := c.Params("id")
+	id, err := strconv.Atoi(strID)
+	if err != nil {
+		return err
+	}
+
+	if err := uh.Store.DeleteUserByID(c.Context(), nil, id); err != nil {
+		return err
+	}
+
 	return nil
 }
