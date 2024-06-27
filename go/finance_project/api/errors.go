ebc7ff98aa066e82795c294fc4c208d241b682ed
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-27 18:06:16 -0500

    go: finance | improved error handling

--- go/finance_project/api/errors.go
+++ go/finance_project/api/errors.go
@@ -1,12 +1,13 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-type ErrorMessage int
+type ErrorCode int
 
 const (
 	_ = iota
@@ -14,10 +15,13 @@ const (
 	NOT_FOUND
 	INVALID_PARAMETERS
 	INVALID_ID
-	INVALID_REQUEST_BODY
+	INVALID_REQUEST
+	INTERNAL_ERROR
+	TOKEN_EXPIRED
+	INVALID_TOKEN
 )
 
-func (e ErrorMessage) String() string {
+func (e ErrorCode) String() string {
 	switch e {
 	case UNAUTHORIZED:
 		return "UNAUTHORIZED"
@@ -27,24 +31,33 @@ func (e ErrorMessage) String() string {
 		return "INVALID_PARAMETERS"
 	case INVALID_ID:
 		return "INVALID_ID"
-	case INVALID_REQUEST_BODY:
-		return "INVALID_REQUEST_BODY"
+	case INVALID_REQUEST:
+		return "INVALID_REQUEST"
+	case INTERNAL_ERROR:
+		return "INTERNAL_ERROR"
+	case TOKEN_EXPIRED:
+		return "TOKEN_EXPIRED"
+	case INVALID_TOKEN:
+		return "INVALID_TOKEN"
 	default:
-		return "Unknown"
+		return "UNKNOWN"
 	}
 }
 
+func (e ErrorCode) MarshalJSON() ([]byte, error) {
+	return json.Marshal(e.String())
+}
+
 type Error struct {
-	Message    string       `json:"msg"` // TODO: Maybe add the original error message
-	ErrMessage ErrorMessage `json:"errMsg"`
-	StatusCode int          `json:"statusCode"`
+	ErrMessage ErrorCode `json:"error"`
+	StatusCode int       `json:"status_code"`
 }
 
 func ErrorHandler(c *fiber.Ctx, err error) error {
 	if apiError, ok := err.(Error); ok {
 		return c.Status(apiError.StatusCode).JSON(apiError)
 	}
-	apiError := NewError(http.StatusInternalServerError, err.Error())
+	apiError := NewError(http.StatusInternalServerError, INTERNAL_ERROR)
 	return c.Status(apiError.StatusCode).JSON(apiError)
 }
 
@@ -52,44 +65,9 @@ func (e Error) Error() string {
 	return e.ErrMessage.String()
 }
 
-func NewError(statusCode int, message string) Error {
+func NewError(statusCode int, error ErrorCode) Error {
 	return Error{
 		StatusCode: statusCode,
-		Message:    message,
-	}
-}
-
-func ErrUnauthorized() Error {
-	return Error{
-		StatusCode: http.StatusUnauthorized,
-		ErrMessage: UNAUTHORIZED,
-	}
-}
-
-func ErrNotFound() Error {
-	return Error{
-		StatusCode: http.StatusNotFound,
-		ErrMessage: NOT_FOUND,
-	}
-}
-
-func ErrInvalidParams() Error {
-	return Error{
-		StatusCode: http.StatusBadRequest,
-		ErrMessage: INVALID_PARAMETERS,
-	}
-}
-
-func ErrInvalidReqBody() Error {
-	return Error{
-		StatusCode: http.StatusBadRequest,
-		ErrMessage: INVALID_REQUEST_BODY,
-	}
-}
-
-func ErrInvalidID() Error {
-	return Error{
-		StatusCode: http.StatusBadRequest,
-		ErrMessage: INVALID_ID,
+		ErrMessage: error,
 	}
 }
