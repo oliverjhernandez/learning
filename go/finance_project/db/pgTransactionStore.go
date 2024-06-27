8a2d9452ec84a732984ce5b87cf83d0210f005d4
oliverjhernandez <oliverjhernandez@gmail.com>
2024-06-27 16:44:13 -0500

    go: finance | add account relation to transaction pg store

--- go/finance_project/db/pgTransactionStore.go
+++ go/finance_project/db/pgTransactionStore.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	"finance/models"
@@ -11,8 +12,8 @@ import (
 
 type TransactionStore interface {
 	InsertTransaction(ctx context.Context, tx *sql.Tx, txn *models.Transaction) (int, error)
-	GetTransactionByID(ctx context.Context, tx *sql.Tx, id int) (*models.Transaction, error)
-	GetAllTransactions(ctx context.Context, tx *sql.Tx) ([]*models.Transaction, error)
+	GetTransactionByID(ctx context.Context, tx *sql.Tx, id int) (models.Transaction, error)
+	GetAllTransactions(ctx context.Context, tx *sql.Tx) ([]models.Transaction, error)
 	UpdateTransaction(ctx context.Context, tx *sql.Tx, id int, params *models.UpdateTransaction) error
 	DeleteTransactionByID(ctx context.Context, tx *sql.Tx, id int) error
 }
@@ -35,10 +36,11 @@ func (s *PGTransactionStore) InsertTransaction(ctx context.Context, tx *sql.Tx, 
 
 	query := `
             INSERT into transactions 
-                (concept, description, value, date, relevance, currency, account, created_at, updated_at)
+                (concept, description, value, date, relevance, account_id, created_at, updated_at)
             values 
-                ($1, $2, $3, $4, $5, $6)
-            returning id`
+                ($1, $2, $3, $4, $5, $6, $7, $8)
+            returning id
+    `
 
 	var err error
 	if tx != nil {
@@ -48,8 +50,7 @@ func (s *PGTransactionStore) InsertTransaction(ctx context.Context, tx *sql.Tx, 
 			txn.Value,
 			txn.Date,
 			txn.Relevance,
-			txn.Currency,
-			txn.Account,
+			txn.AccountID,
 			time.Now(),
 			time.Now(),
 		).Scan(&newID)
@@ -60,8 +61,7 @@ func (s *PGTransactionStore) InsertTransaction(ctx context.Context, tx *sql.Tx, 
 			txn.Value,
 			txn.Date,
 			txn.Relevance,
-			txn.Currency,
-			txn.Account,
+			txn.AccountID,
 			time.Now(),
 			time.Now(),
 		).Scan(&newID)
@@ -70,10 +70,10 @@ func (s *PGTransactionStore) InsertTransaction(ctx context.Context, tx *sql.Tx, 
 		return 0, err
 	}
 
-	return 0, nil
+	return newID, nil
 }
 
-func (s *PGTransactionStore) GetTransactionByID(ctx context.Context, tx *sql.Tx, id int) (*models.Transaction, error) {
+func (s *PGTransactionStore) GetTransactionByID(ctx context.Context, tx *sql.Tx, id int) (models.Transaction, error) {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
 	defer cancel()
 
@@ -81,85 +81,90 @@ func (s *PGTransactionStore) GetTransactionByID(ctx context.Context, tx *sql.Tx,
 
 	query := `
             SELECT 
-                (concept, description, value, date, relevance, currency, account, created_at, updated_at)
-            from transactions
-            WHERE
-            id=$1`
+                id, concept, description, value, date, relevance, account_id, created_at, updated_at
+            FROM transactions
+            WHERE id=$1
+    `
 
 	var err error
 	if tx != nil {
-		err = s.client.QueryRowContext(ctx, query, id).Scan(
+		err = tx.QueryRowContext(ctx, query, id).Scan(
 			&txn.ID,
+			&txn.Concept,
 			&txn.Description,
 			&txn.Value,
 			&txn.Date,
 			&txn.Relevance,
-			&txn.Currency,
-			&txn.Account,
+			&txn.AccountID,
 			&txn.CreatedAt,
 			&txn.UpdatedAt,
 		)
 	} else {
 		err = s.client.QueryRowContext(ctx, query, id).Scan(
 			&txn.ID,
+			&txn.Concept,
 			&txn.Description,
 			&txn.Value,
 			&txn.Date,
 			&txn.Relevance,
-			&txn.Currency,
-			&txn.Account,
+			&txn.AccountID,
 			&txn.CreatedAt,
 			&txn.UpdatedAt,
 		)
 	}
 	if err != nil {
-		return &txn, err
+		fmt.Printf("error: %v", err)
+		return txn, err
 	}
 
-	return &txn, nil
+	return txn, nil
 }
 
-func (s *PGTransactionStore) GetAllTransactions(ctx context.Context, tx *sql.Tx) ([]*models.Transaction, error) {
+func (s *PGTransactionStore) GetAllTransactions(ctx context.Context, tx *sql.Tx) ([]models.Transaction, error) {
 	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
 	defer cancel()
 
-	var txs []*models.Transaction
+	var txns []models.Transaction
 
 	query := `
-            SELECT (id, first_name, last_name, email, password, created_at, updated_at)
-            from transactions`
+            SELECT 
+                id, concept, description, value, date, relevance, account_id, created_at, updated_at
+            from transactions
+    `
 
-	var rows *sql.Rows
 	var err error
+	var rows *sql.Rows
 	if tx != nil {
 		rows, err = tx.QueryContext(ctx, query)
 	} else {
 		rows, err = s.client.QueryContext(ctx, query)
 	}
-
 	if err != nil {
-		return txs, err
+		return txns, err
 	}
+	defer rows.Close()
 
 	for rows.Next() {
-		var tx models.Transaction
-		err := s.client.QueryRowContext(ctx, query).Scan(
-			&tx.ID,
-			&tx.Description,
-			&tx.Value,
-			&tx.Date,
-			&tx.Relevance,
-			&tx.Currency,
-			&tx.Account,
-			&tx.CreatedAt,
-			&tx.UpdatedAt,
+		var txn models.Transaction
+		err = rows.Scan(
+			&txn.ID,
+			&txn.Concept,
+			&txn.Description,
+			&txn.Value,
+			&txn.Date,
+			&txn.Relevance,
+			&txn.AccountID,
+			&txn.CreatedAt,
+			&txn.UpdatedAt,
 		)
 		if err != nil {
-			return txs, err
+			return nil, err
 		}
+
+		txns = append(txns, txn)
 	}
 
-	return txs, nil
+	return txns, nil
 }
 
 func (s *PGTransactionStore) UpdateTransaction(ctx context.Context, tx *sql.Tx, id int, params *models.UpdateTransaction) error {
@@ -168,45 +173,58 @@ func (s *PGTransactionStore) UpdateTransaction(ctx context.Context, tx *sql.Tx, 
 
 	now := time.Now()
 
+	setClauses := []string{}
+	args := []interface{}{}
+	argID := 1
+
+	if params.Concept != "" {
+		setClauses = append(setClauses, fmt.Sprintf("concept = $%d", argID))
+		args = append(args, params.Concept)
+		argID++
+	}
+	if params.Description != "" {
+		setClauses = append(setClauses, fmt.Sprintf("description = $%d", argID))
+		args = append(args, params.Description)
+		argID++
+	}
+	if !params.Date.IsZero() {
+		setClauses = append(setClauses, fmt.Sprintf("date = $%d", argID))
+		args = append(args, params.Date)
+		argID++
+	}
+	if params.Relevance != 0 {
+		setClauses = append(setClauses, fmt.Sprintf("relevance = $%d", argID))
+		args = append(args, params.Relevance)
+		argID++
+	}
+	if params.AccountID != 0 {
+		setClauses = append(setClauses, fmt.Sprintf("account_id = $%d", argID))
+		args = append(args, params.AccountID)
+		argID++
+	}
+	if params.Value != 0 {
+		setClauses = append(setClauses, fmt.Sprintf("value = $%d", argID))
+		args = append(args, params.Value)
+		argID++
+	}
+
+	setClauses = append(setClauses, fmt.Sprintf("updated_at = $%d", argID))
+	args = append(args, now)
+	argID++
+
+	args = append(args, id)
+
 	query := fmt.Sprintf(`
-        UPDATE transactions set 
-            concept = COALESCE($1, concept)
-            description = COALESCE($2, description)
-            value = COALESCE($3, value)
-            date = COALESCE($4, date)
-            relevance = COALESCE($5, relevance)
-            currency = COALESCE($6, currency)
-            account = COALESCE($7, account)
-            updated_at = %s
-        WHERE
-            id = $8
-    `, now)
+        UPDATE transactions 
+        SET %s
+        WHERE id = $%d
+    `, strings.Join(setClauses, ", "), argID)
 
 	var err error
 	if tx != nil {
-		_, err = s.client.ExecContext(
-			ctx,
-			query,
-			params.Concept,
-			params.Description,
-			params.Value,
-			params.Date,
-			params.Relevance,
-			params.Currency,
-			params.Account,
-			id)
+		_, err = tx.ExecContext(ctx, query, args...)
 	} else {
-		_, err = s.client.ExecContext(
-			ctx,
-			query,
-			params.Concept,
-			params.Description,
-			params.Value,
-			params.Date,
-			params.Relevance,
-			params.Currency,
-			params.Account,
-			id)
+		_, err = s.client.ExecContext(ctx, query, args...)
 	}
 	if err != nil {
 		return err
