65ee715dcbadf11112216c88790abd374507c2c6
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-10 17:08:25 -0500

    green: integrate pq

--- go/greenlight/cmd/api/main.go
+++ go/greenlight/cmd/api/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	env  string
 	port int
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -24,9 +34,22 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 3000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stag|prod)")
+
+	cfg.db.dsn = "user=postgres password=secret host=localhost dbname=greenlight sslmode=disable"
+	cfg.db.maxOpenConns = 25
+	cfg.db.maxIdleConns = 25
+	cfg.db.maxIdleTime = "15m"
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -42,6 +65,33 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(c config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", c.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(c.db.maxOpenConns)
+	db.SetMaxIdleConns(c.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(c.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*5)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
