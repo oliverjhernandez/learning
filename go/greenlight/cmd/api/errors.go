cc4a940051a04781b28eb00a966bcd9adaa70586
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-11 20:16:44 -0500

    green: add editConflict error

--- go/greenlight/cmd/api/errors.go
+++ go/greenlight/cmd/api/errors.go
@@ -21,29 +21,30 @@ func (a *application) errorResponse(w http.ResponseWriter, r *http.Request, stat
 }
 
 func (a *application) internalServerError(w http.ResponseWriter, r *http.Request) {
-	// a.logError(r, err)
 	message := "the server encountered a problem and could not process your request"
 	a.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
 func (a *application) notFoundError(w http.ResponseWriter, r *http.Request) {
-	// a.logError(r, err)
 	message := "the requested resource could not be found"
 	a.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 func (a *application) methodNotAllowedError(w http.ResponseWriter, r *http.Request) {
-	// a.logError(r, err)
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	a.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
 
 func (a *application) badRequestError(w http.ResponseWriter, r *http.Request) {
-	// a.logError(r, err)
 	message := "the server was unable to process the request"
 	a.errorResponse(w, r, http.StatusNotFound, message)
 }
 
 func (a *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	a.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (a *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	a.errorResponse(w, r, http.StatusConflict, message)
 }
