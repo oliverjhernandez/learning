7f251cd89ea1061d0f8911d3cb037f02dbe9be89
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-11 20:16:23 -0500

    green: add missing method endpoints

--- go/greenlight/cmd/api/routes.go
+++ go/greenlight/cmd/api/routes.go
@@ -12,9 +12,11 @@ func (a *application) routes() *httprouter.Router {
 	router.NotFound = http.HandlerFunc(a.notFoundError)
 	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedError)
 
-	router.HandlerFunc("GET", "/v1/health", a.healthHandler)
-	router.HandlerFunc("POST", "/v1/movie", a.createMovieHandler)
-	router.HandlerFunc("GET", "/v1/movie/:id", a.getMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/health", a.healthHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movie", a.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", a.getMovieHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/movie/:id", a.updateMovieHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/movie/:id", a.deleteMovieHandler)
 
 	return router
 }
