d698a867acf1ee28ff90f09cb3812a4aff0e34b3
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-16 11:16:38 -0500

    green: cleaner movie validation

--- go/greenlight/internal/data/movie.go
+++ go/greenlight/internal/data/movie.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"greenlight/internal/validator"
@@ -190,23 +191,23 @@ type Movie struct {
 	Version   int32     `json:"version"`
 }
 
-func ValidateMovie(v *validator.Validator, movie *Movie) {
+func ValidateMovie(v *validator.Validator, m *Movie) {
 	// Title
-	v.Check(movie.Title != "", "title", "must be provided")
-	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(m.Title != "", "title", "must be provided")
+	v.Check(len(m.Title) <= 500, "title", "must not be more than 500 bytes long")
 
 	// Year
-	v.Check(movie.Year != 0, "year", "must be provided")
-	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(m.Year != 0, "year", "must be provided")
+	v.Check(m.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(m.Year <= int32(time.Now().Year()), "year", "must not be in the future")
 
 	// Runtime
-	v.Check(movie.Runtime != 0, "runtime", "must be provided")
-	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(m.Runtime != 0, "runtime", "must be provided")
+	v.Check(m.Runtime > 0, "runtime", "must be a positive integer")
 
 	// Genres
-	v.Check(movie.Genres != nil, "genres", "must be provided")
-	v.Check(len(movie.Genres) >= 1, "genres", "at least one genre must be provided")
-	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
-	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+	v.Check(m.Genres != nil, "genres", "must be provided")
+	v.Check(len(m.Genres) >= 1, "genres", "at least one genre must be provided")
+	v.Check(len(m.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(m.Genres), "genres", "must not contain duplicate values")
 }
