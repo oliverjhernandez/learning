7712939eb0716d9d81d2e0061b1808e6e86fc3bd
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-16 17:25:42 -0500

    casita: renamed errors method

--- go/casita/cmd/api/errors.go
+++ go/casita/cmd/api/errors.go
@@ -8,48 +8,47 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func NewError(c *fiber.Ctx, status int, error string) {
+func NewApiError(c *fiber.Ctx, status int, error string) {
 	err := Envelope{
 		Status: strconv.Itoa(status),
 		Error:  error,
 	}
 
 	c.Response().Header.Add("Content-Type", "application/json")
-
 	c.Status(status).JSON(err)
 }
 
 func internalServerError(c *fiber.Ctx) {
 	message := "the server encountered a problem and could not process your request"
-	NewError(c, http.StatusInternalServerError, message)
+	NewApiError(c, http.StatusInternalServerError, message)
 }
 
 func notFoundError(c *fiber.Ctx) {
 	message := "the resource you requested could not be found"
-	NewError(c, http.StatusNotFound, message)
+	NewApiError(c, http.StatusNotFound, message)
 }
 
 func methodNotAllowedError(c *fiber.Ctx) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", c.Method())
-	NewError(c, http.StatusMethodNotAllowed, message)
+	NewApiError(c, http.StatusMethodNotAllowed, message)
 }
 
 func badRequestError(c *fiber.Ctx) {
 	message := "the server was unable to process the request"
-	NewError(c, http.StatusBadRequest, message)
+	NewApiError(c, http.StatusBadRequest, message)
 }
 
 func editConflictError(c *fiber.Ctx) {
 	message := "unable to update the record due to an edit conflict, please try again"
-	NewError(c, http.StatusConflict, message)
+	NewApiError(c, http.StatusConflict, message)
 }
 
 func unauthorizedError(c *fiber.Ctx) {
 	message := "unauthorized access"
-	NewError(c, http.StatusUnauthorized, message)
+	NewApiError(c, http.StatusUnauthorized, message)
 }
 
 func invalidCredentials(c *fiber.Ctx) {
 	message := "invalid credentials"
-	NewError(c, http.StatusUnauthorized, message)
+	NewApiError(c, http.StatusUnauthorized, message)
 }
