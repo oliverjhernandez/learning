055e00e827a4714fadda71a2bad69018b8b3babc
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-16 11:18:27 -0500

    green: adds rateLimit error response

--- go/greenlight/cmd/api/errors.go
+++ go/greenlight/cmd/api/errors.go
@@ -50,3 +50,8 @@ func (a *application) editConflictResponse(w http.ResponseWriter, r *http.Reques
 	message := "unable to update the record due to an edit conflict, please try again"
 	a.errorResponse(w, r, http.StatusConflict, message)
 }
+
+func (a *application) rateLimitExeededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exeeded"
+	a.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
