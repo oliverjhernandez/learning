a6088950339b853ca8df5d577cb1c6cb9417e96e
oliverjhernandez <oliverjhernandez@gmail.com>
2024-07-17 11:46:49 -0500

    casita: use new error helpers in account handlers

--- go/casita/cmd/api/account_handler.go
+++ go/casita/cmd/api/account_handler.go
@@ -20,95 +20,122 @@ func NewAccountHandler(s *db.Store) *AccountHandler {
 	}
 }
 
-func (ah *AccountHandler) HandlerPostAccount(c *fiber.Ctx) {
-	var params *models.CreateAccount
+var params *models.CreateAccount
+
+func (ah *AccountHandler) HandlerPostAccount(c *fiber.Ctx) error {
 	if err := c.BodyParser(&params); err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	acc, err := models.NewAccountFromParams(params)
 	if err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	acc, err = ah.Store.InsertAccount(c.Context(), nil, acc)
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
 
 	err = writeJSON(c, http.StatusOK, "resource created successfully", acc, "")
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
+
+	return nil
 }
 
-func (ah *AccountHandler) HandlerGetAccounts(c *fiber.Ctx) {
+func (ah *AccountHandler) HandlerGetAccounts(c *fiber.Ctx) error {
 	accs, err := ah.Store.GetAllAccounts(c.Context(), nil)
 	if err != nil {
 		notFoundError(c)
+		return err
 	}
 
 	err = writeJSON(c, http.StatusOK, "got you", accs, "")
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
+
+	return nil
 }
 
-func (ah *AccountHandler) HandlerGetAccount(c *fiber.Ctx) {
+func (ah *AccountHandler) HandlerGetAccount(c *fiber.Ctx) error {
 	strID := c.Params("id")
 	id, err := strconv.Atoi(strID)
 	if err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	acc, err := ah.Store.GetAccountByID(c.Context(), nil, id)
 	if err != nil {
 		notFoundError(c)
+		return err
 	}
 
 	err = writeJSON(c, http.StatusOK, "got you", acc, "")
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
+
+	return nil
 }
 
-func (ah *AccountHandler) HandlerUpdateAccount(c *fiber.Ctx) {
+func (ah *AccountHandler) HandlerUpdateAccount(c *fiber.Ctx) error {
 	var params models.UpdateAccount
 
 	strID := c.Params("id")
 	id, err := strconv.Atoi(strID)
 	if err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	if err := c.BodyParser(&params); err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	acc, err := ah.Store.UpdateAccount(c.Context(), nil, id, &params)
 	if err != nil {
 		editConflictError(c)
+		return err
 	}
 
 	err = writeJSON(c, http.StatusOK, "updated successfully", acc, "")
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
+
+	return nil
 }
 
-func (ah *AccountHandler) HandlerDeleteAccount(c *fiber.Ctx) {
+func (ah *AccountHandler) HandlerDeleteAccount(c *fiber.Ctx) error {
 	strID := c.Params("id")
 	id, err := strconv.Atoi(strID)
 	if err != nil {
 		badRequestError(c)
+		return err
 	}
 
 	if err := ah.Store.DeleteAccountByID(c.Context(), nil, id); err != nil {
 		internalServerError(c)
+		return err
 	}
 
 	err = writeJSON(c, http.StatusOK, "resource deleted", nil, "")
 	if err != nil {
 		internalServerError(c)
+		return err
 	}
+
+	return nil
 }
