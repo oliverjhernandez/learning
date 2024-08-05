b4337be7499b9bcc6dcdec2dc15e61bb648ea2ca
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-05 17:04:00 -0500

    casita: add user validation helper

--- go/casita/internal/models/user.go
+++ go/casita/internal/models/user.go
@@ -4,6 +4,8 @@ import (
 	"net/mail"
 	"time"
 
+	"casita/internal/validator"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,7 +20,7 @@ type User struct {
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
 	Passwd    string    `json:"-"`
-	IsAdmin   bool      `json:"is_admin"`
+	IsAdmin   *bool     `json:"is_admin"`
 	ID        int       `json:"id"`
 }
 
@@ -27,14 +29,14 @@ type CreateUser struct {
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Passwd    string `json:"passwd"`
-	IsAdmin   bool   `json:"is_admin"`
+	IsAdmin   *bool  `json:"is_admin"`
 }
 
 type UpdateUser struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
-	IsAdmin   bool   `json:"is_admin"`
+	IsAdmin   *bool  `json:"is_admin"`
 }
 
 func isValidEmail(email string) bool {
@@ -62,4 +64,22 @@ func NewUserFromParams(u *CreateUser) (*User, error) {
 		Passwd:    string(encpw),
 		Email:     u.Email,
 	}, nil
+}
+
+func ValidateUser(v *validator.Validator, u *User) {
+	// FirstName
+	v.Check(u.FirstName != "", "first_name", "must be provided")
+	v.Check(len(u.FirstName) >= 3, "first_name", "must be at least 3 bytes long")
+
+	// LastName
+	v.Check(u.LastName != "", "last_name", "must be provided")
+	v.Check(len(u.LastName) >= 3, "last_name", "must be at least 3 bytes long")
+
+	// Is_Admin
+	v.Check(u.IsAdmin != nil, "is_admin", "must be provided")
+
+	// Passwd
+	v.Check(u.Passwd != "", "passwd", "must be provided")
+	v.Check(len(u.Passwd) >= 60, "passwd", "must be at least 6 bytes long")
+	v.Check(len(u.Passwd) <= 100, "passwd", "must not be longer than 30 bytes")
 }
