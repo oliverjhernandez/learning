bc3657e2b6664742136b00778513580399432b93
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-05 17:03:45 -0500

    casita: update closing and due days fields in credit

--- go/casita/internal/models/credit.go
+++ go/casita/internal/models/credit.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"casita/internal/validator"
+)
 
 type Entity int
 
@@ -46,36 +50,11 @@ func (t Type) String() string {
 	}
 }
 
-type UpdateCredit struct {
-	UpdatedAt    time.Time `json:"updated_at"`
-	ClosingDate  time.Time `json:"closing_date"`
-	DueDate      time.Time `json:"due_date"`
-	Identifier   string    `json:"identifier"`
-	Entity       Entity    `json:"entity"`
-	Type         Type      `json:"type"`
-	Rate         float32   `json:"rate"`
-	Total        int32     `json:"total"`
-	Installments int8      `json:"installments"`
-	UserID       int       `json:"user_id"`
-}
-
-type CreateCredit struct {
-	ClosingDate  time.Time `json:"closing_date"`
-	DueDate      time.Time `json:"due_date"`
-	Identifier   string    `json:"identifier"`
-	Entity       Entity    `json:"entity"`
-	Type         Type      `json:"type"`
-	Rate         float32   `json:"rate"`
-	Total        int32     `json:"total"`
-	Installments int8      `json:"installments"`
-	UserID       int       `json:"user_id"`
-}
-
 type Credit struct {
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
-	ClosingDate  time.Time `json:"closing_date"`
-	DueDate      time.Time `json:"due_date"`
+	ClosingDay   int8      `json:"closing_day"`
+	DueDay       int8      `json:"due_day"`
 	Identifier   string    `json:"identifier"`
 	Entity       Entity    `json:"entity"`
 	Type         Type      `json:"type"`
@@ -86,20 +65,79 @@ type Credit struct {
 	Installments int8      `json:"installments"`
 }
 
+type UpdateCredit struct {
+	UpdatedAt    time.Time `json:"updated_at"`
+	ClosingDay   int8      `json:"closing_day"`
+	DueDay       int8      `json:"due_day"`
+	Identifier   string    `json:"identifier"`
+	Entity       Entity    `json:"entity"`
+	Type         Type      `json:"type"`
+	Rate         float32   `json:"rate"`
+	Total        int32     `json:"total"`
+	Installments int8      `json:"installments"`
+	UserID       int       `json:"user_id"`
+}
+
+type CreateCredit struct {
+	ClosingDay   int8    `json:"closing_day"`
+	DueDay       int8    `json:"due_day"`
+	Identifier   string  `json:"identifier"`
+	Entity       Entity  `json:"entity"`
+	Type         Type    `json:"type"`
+	Rate         float32 `json:"rate"`
+	Total        int32   `json:"total"`
+	Installments int8    `json:"installments"`
+	UserID       int     `json:"user_id"`
+}
+
 func NewCreditFromParams(p *CreateCredit) (*Credit, error) {
 	now := time.Now()
 
 	return &Credit{
 		CreatedAt:    now,
 		UpdatedAt:    now,
-		ClosingDate:  p.ClosingDate,
-		DueDate:      p.DueDate,
-		Identifier:   p.Identifier,
-		Entity:       p.Entity,
-		Type:         p.Type,
-		Rate:         p.Rate,
-		Total:        p.Total,
-		Installments: p.Installments,
-		UserID:       p.UserID,
+		ClosingDay:   p.ClosingDay,   // Billing cycle end date
+		DueDay:       p.DueDay,       // Limit date to pay at least a min
+		Identifier:   p.Identifier,   // Credit identifier, usually provideed by the entity
+		Entity:       p.Entity,       // Bank, Person, any institution that granted the credit
+		Type:         p.Type,         // enum makes it self-explenatory
+		Rate:         p.Rate,         // Interest rate
+		Total:        p.Total,        // Total granted for the credit
+		Installments: p.Installments, // Top number of payments to finalize the credit
+		UserID:       p.UserID,       // Owner of the credit
 	}, nil
+}
+
+func ValidateCredit(v *validator.Validator, c *Credit) {
+	// ClosingDate
+	v.Check(c.ClosingDay > 0, "closing_day", "must be greater than zero")
+	v.Check(c.ClosingDay <= 30, "closing_day", "must be lower or equal to 30")
+	v.Check(c.ClosingDay <= c.DueDay, "closing_day", "must take place before due_day")
+
+	// DueDate
+	v.Check(c.DueDay > 0, "due_day", "must be greater than zero")
+	v.Check(c.DueDay <= 30, "due_day", "must be lower or equal to zero")
+	v.Check(c.DueDay >= c.ClosingDay, "due_date", "must take place after closing_day")
+
+	// Identifier
+	v.Check(c.Identifier != "", "identifier", "must be provided")
+	v.Check(len(c.Identifier) >= 5, "identifier", "must be at least 5 bytes long")
+
+	// Entity
+	v.Check(c.Entity != 0, "entity", "must be provided")
+
+	// Type
+	v.Check(c.Type != 0, "passwd", "must be provided")
+
+	// Rate
+	v.Check(c.Rate >= 0, "rate", "must be provided")
+
+	// Total
+	v.Check(c.Total > 0, "total", "must be greater than zero")
+
+	// Installments
+	v.Check(c.Installments > 0, "installments", "must be greater than zero")
+
+	// UserID
+	v.Check(c.UserID != 0, "user_id", "must be assigned to a user")
 }
