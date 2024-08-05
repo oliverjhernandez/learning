5eeb80c47b47881e35566b7944665d5e9ab853a3
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-05 17:04:20 -0500

    greenlight: format

--- go/greenlight/internal/data/movie.go
+++ go/greenlight/internal/data/movie.go
@@ -130,12 +130,12 @@ func (m *MovieModel) Delete(id int) error {
 
 func (m *MovieModel) GetAll(title string, genres []string, filter Filters) ([]*Movie, Metadata, error) {
 	query := fmt.Sprintf(`
-  SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
-  FROM movies
-  WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
-    AND (genres @> $2 OR $2 = '{}')
-  ORDER BY %s %s, id ASC
-  LIMIT $3 OFFSET $4`, filter.sortColumn(), filter.sortDirection())
+    SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
+    FROM movies
+    WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+      AND (genres @> $2 OR $2 = '{}')
+    ORDER BY %s %s, id ASC
+    LIMIT $3 OFFSET $4`, filter.sortColumn(), filter.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
