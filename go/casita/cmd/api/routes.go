42eaca0b67c89d8293e5f52c918cd9bbd95884a6
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-13 17:08:01 -0500

    casita: renamed list endpoints

--- go/casita/cmd/api/routes.go
+++ go/casita/cmd/api/routes.go
@@ -24,28 +24,28 @@ func Routes(stores *db.Store, app *fiber.App) {
 	api.Post("/auth", authHandler.HandleAuthenticate)
 
 	// Transaction CRUD Endpoints
-	appv1.Get("/transaction", txHandler.HandlerGetTransactions)
+	appv1.Get("/transactions", txHandler.HandlerGetTransactions)
 	appv1.Get("/transaction/:id", txHandler.HandlerGetTransaction)
 	appv1.Post("/transaction", txHandler.HandlerPostTransaction)
 	appv1.Delete("/transaction/:id", txHandler.HandlerDeleteTransaction)
 	appv1.Patch("/transaction/:id", txHandler.HandlerUpdateTransaction)
 
 	// Credit CRUD Endpoints
-	appv1.Get("/credit", creditHandler.HandlerGetCredits)
+	appv1.Get("/credits", creditHandler.HandlerGetCredits)
 	appv1.Get("/credit/:id", creditHandler.HandlerGetCredit)
 	appv1.Post("/credit", creditHandler.HandlerPostCredit)
 	appv1.Delete("/credit/:id", creditHandler.HandlerDeleteCredit)
 	appv1.Patch("/credit/:id", creditHandler.HandlerUpdateCredit)
 
 	// User CRUD Endpoints
-	appv1.Get("/user", userHandler.HandlerGetUsers)
+	appv1.Get("/users", userHandler.HandlerGetUsers)
 	appv1.Get("/user/:id", userHandler.HandlerGetUser)
 	appv1.Post("/user", userHandler.HandlerPostUser)
 	appv1.Delete("/user/:id", userHandler.HandlerDeleteUser)
 	appv1.Patch("/user/:id", userHandler.HandlerUpdateUser)
 
 	// Account CRUD Endpoints
-	appv1.Get("/account", accountHandler.HandlerGetAccounts)
+	appv1.Get("/accounts", accountHandler.HandlerGetAccounts)
 	appv1.Get("/account/:id", accountHandler.HandlerGetAccount)
 	appv1.Post("/account", accountHandler.HandlerPostAccount)
 	appv1.Delete("/account/:id", accountHandler.HandlerDeleteAccount)
