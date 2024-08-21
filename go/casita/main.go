0b63f9e705490a05211c0b5ea5b2701055d8d506
oliverjhernandez <oliverjhernandez@gmail.com>
2024-08-20 20:26:55 -0500

    chore: relocated some code

--- go/casita/main.go
+++ go/casita/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	"os"
 
 	"casita/cmd/api"
 	"casita/internal/db"
@@ -10,18 +12,94 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type dbParams struct {
+	host   string
+	port   string
+	name   string
+	user   string
+	passwd string
+	ssl    string
+
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
+type config struct {
+	env     string
+	port    int
+	db      dbParams
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
+}
+
+type app struct {
+	logger *jsonlog.Logger
+	config config
+}
+
 func main() {
 	stores, client, err := db.NewStore()
+	cfg := config{
+		env:  "dev",
+		port: 4000,
+		db: dbParams{
+			host:   "localhost",
+			port:   "5432",
+			name:   "casita",
+			user:   "postgres",
+			passwd: "secret",
+			ssl:    "disable",
+		},
+	}
+
+	app := &app{
+		logger: logger,
+		config: cfg,
+	}
+
+	client, err := connectSQL(app.config.db)
 	if err != nil {
 		fmt.Printf("error loading db store")
 	}
 	defer client.Close()
 
+	stores := &db.Store{
+		DB:           client,
+		UserStore:    db.NewPGUserStore(client),
+		TxnStore:     db.NewPGTransactionStore(client),
+		CreditStore:  db.NewPGCreditStore(client),
+		AccountStore: db.NewPGAccountStore(client),
+	}
+
 	fiberConfig := fiber.Config{ErrorHandler: api.ErrorHandler}
 	fiberApp := fiber.New(fiberConfig)
 	listenAddr := flag.String("listenAddr", ":4000", "The listen address of the API server")
 
-	api.Routes(stores, fiberApp)
+	api.InitializeRoutes(stores, fiberApp)
 
 	fiberApp.Listen(*listenAddr)
+}
+
+func connectSQL(dbParams dbParams) (*sql.DB, error) {
+	connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s",
+		dbParams.host,
+		dbParams.port,
+		dbParams.name,
+		dbParams.user,
+		dbParams.passwd,
+		dbParams.ssl)
+
+	db, err := sql.Open("pgx", connectionString)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
