66836358fcfe7408a51bf7de14788b0f083d6887
oliverjhernandez <oliverjhernandez@gmail.com>
2024-11-04 11:51:29 -0400

    [casita] format

--- go/casita/migrations/000001_create_users_table.up.sql
+++ go/casita/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,11 @@
-CREATE TABLE "users" (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
-    passwd VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+  id bigserial PRIMARY KEY,
+  created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW(),
+  first_name text NOT NULL,
+  last_name text NOT NULL,
+  email citext UNIQUE NOT NULL,
+  password_hash bytea NOT NULL,
+  version integer NOT NULL DEFAULT 1,
+  activated bool NOT NULL
 );
