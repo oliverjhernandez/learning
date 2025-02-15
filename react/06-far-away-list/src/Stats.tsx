4aefddf3ed383d54e474c80c6e070bf577d6759d
oliverjhernandez <oliverjhernandez@gmail.com>
2025-02-15 14:19:30 -0400

    [react]: finish far away list

--- react/06-far-away-list/src/Stats.tsx
+++ react/06-far-away-list/src/Stats.tsx
@@ -1,9 +1,30 @@
 import "./index.css";
+import { itemProp } from "./PackingList";
 
-const Stats = () => {
+type StatsProps = {
+  items: itemProp[];
+};
+
+const Stats = (props: StatsProps) => {
+  if (props.items.length === 0) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+  }
+  const numItems = props.items.length;
+  const numCheckedItems = props.items.filter((i) => i.packed).length;
+  const perctCheckedItems = Math.round((numCheckedItems / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list and you already packed X (X%)</em>
+      <em>
+        {perctCheckedItems === 100
+          ? "You got everything! Ready to go ✈️ "
+          : `🧳 You have ${numItems} items on your list and you already packed
+        ${numCheckedItems} (${perctCheckedItems}%)`}
+      </em>
     </footer>
   );
 };
