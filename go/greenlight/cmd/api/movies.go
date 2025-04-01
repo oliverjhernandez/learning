18059bb19e2dceb8eebc4bc2edc9639f082b7296
oliverjhernandez <oliverjhernandez@gmail.com>
2025-04-01 11:55:12 -0400

    [greenlight]: add validator for createMovie handler

--- go/greenlight/cmd/api/movies.go
+++ go/greenlight/cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"greenlight/internal/data"
+	"greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 	}
 
 	fmt.Fprintf(w, "%+v\n", input)
