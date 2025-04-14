e29887eb452da90a4abcb494f45ed6a88295eb1a
oliverjhernandez <oliverjhernandez@gmail.com>
2025-04-14 11:19:10 -0400

    [greenlight]: modernize code
    [greenlight]: fix title field in accounts

--- go/greenlight/internal/data/movies.go
+++ go/greenlight/internal/data/movies.go
@@ -112,7 +112,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	args := []interface{}{title, pq.Array(genres), filters.limit(), filters.offset()}
+	args := []any{title, pq.Array(genres), filters.limit(), filters.offset()}
 
 	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
