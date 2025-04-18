2f5aee0629a228350c64f5a37b74c930c3f2a588
oliverjhernandez <oliverjhernandez@gmail.com>
2025-04-18 11:55:12 -0400

    [greenlight]: move routes to standard library
    [greenlight]: fix title field in accounts

--- go/greenlight/cmd/api/routes.go
+++ go/greenlight/cmd/api/routes.go
@@ -2,41 +2,49 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routes() http.Handler {
-	router := httprouter.New()
+	mux := http.NewServeMux()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	// TODO: Move this to standard library
+	// router := httprouter.New()
+	// router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	// router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	// healthcheck
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 
 	// movies
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission("movies:write", app.createMovieHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requirePermission("movies:read", app.showMovieHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requirePermission("movies:read", app.listMoviesHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requirePermission("movies:write", app.updateMovieHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requirePermission("movies:write", app.deleteMovieHandler))
+	mux.HandleFunc("POST /v1/movies", app.requirePermission("movies:write", app.createMovieHandler))
+	mux.HandleFunc("GET /v1/movies/{id}", app.requirePermission("movies:read", app.showMovieHandler))
+	mux.HandleFunc("GET /v1/movies", app.requirePermission("movies:read", app.listMoviesHandler))
+	mux.HandleFunc("PATCH /v1/movies/{id}", app.requirePermission("movies:write", app.updateMovieHandler))
+	mux.HandleFunc("DELETE /v1/movies/{id}", app.requirePermission("movies:write", app.deleteMovieHandler))
 
 	// accounts
-	router.HandlerFunc(http.MethodPost, "/v1/accounts", app.requirePermission("accounts:write", app.createAccountHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/accounts/:id", app.requirePermission("accounts:read", app.showAccountHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/accounts", app.requirePermission("accounts:read", app.listAccountsHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/accounts/:id", app.requirePermission("accounts:write", app.updateAccountHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/accounts/:id", app.requirePermission("accounts:write", app.deleteAccountHandler))
+	mux.HandleFunc("POST /v1/accounts", app.requirePermission("accounts:write", app.createAccountHandler))
+	mux.HandleFunc("GET /v1/accounts/{id}", app.requirePermission("accounts:read", app.showAccountHandler))
+	mux.HandleFunc("GET /v1/accounts", app.requirePermission("accounts:read", app.listAccountsHandler))
+	mux.HandleFunc("PATCH /v1/accounts/{id}", app.requirePermission("accounts:write", app.updateAccountHandler))
+	mux.HandleFunc("DELETE /v1/accounts/{id}", app.requirePermission("accounts:write", app.deleteAccountHandler))
 
 	// users
-	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
-	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	mux.HandleFunc("POST /v1/users", app.registerUserHandler)
+	mux.HandleFunc("PUT /v1/users/activated", app.activateUserHandler)
 
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	mux.HandleFunc("POST /v1/tokens/activation", app.createActivationTokenHandler)
+	mux.HandleFunc("POST /v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	// router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
-	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
+	return app.metrics(
+		app.recoverPanic(
+			app.enableCORS(
+				app.rateLimit(
+					app.authenticate(mux),
+				),
+			),
+		),
+	)
 }
