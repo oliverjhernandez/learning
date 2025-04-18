b168de1ef18aa6db4877d786a1ad7f93e400be8b
oliverjhernandez <oliverjhernandez@gmail.com>
2025-04-18 11:54:49 -0400

    [greenlight]: update readIDParam to get id using standard library
    [greenlight]: fix title field in accounts

--- go/greenlight/cmd/api/helpers.go
+++ go/greenlight/cmd/api/helpers.go
@@ -4,24 +4,23 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"greenlight/internal/validator"
 	"io"
 	"maps"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
-
-	"greenlight/internal/validator"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-type envelope map[string]any
+type (
+	envelope map[string]any
+)
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
+	id_param := r.PathValue("id")
 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := strconv.ParseInt(id_param, 10, 64)
 
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
